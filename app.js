bfedf45dde03e814a55609daf9ad43bd2d626e8d
Taral Pawar <taralpawar@gmail.com>
2020-04-23 13:45:18 +0530

    updated

---
+++ app.js
@@ -0,0 +1,35 @@
+(function () {
+  "use strict";
+  angular
+    .module("myFirstApp", [])
+    .controller("myController", controllerFunction);
+
+  controllerFunction.$inject = ["$scope"];
+  function controllerFunction($scope) {
+    $scope.lunchItems = "";
+    $scope.message = "";
+
+    $scope.checkLunch = function () {
+      const str = $scope.lunchItems;
+      if (str == "") {
+        $scope.message = "Enter atleast one item";
+      } else {
+        const items = str.split(",");
+        console.log(items);
+        var num = 0;
+
+        for (var i = 0; i < items.length; i++) {
+          if (items[i] != "" && items[i] != " ") {
+            num += 1;
+          }
+        }
+        console.log(num);
+        if (num > 3) {
+          $scope.message = "Too much!";
+        } else {
+          $scope.message = "Enjoy!";
+        }
+      }
+    };
+  }
+})();
